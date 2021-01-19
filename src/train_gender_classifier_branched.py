cad4afd9fc0762c244cfa1f6080e2dcc78bd05df
nml015 <nml015@nml015-2868.pods.nml015.svc.cluster.local>
2021-01-19 03:11:24 -0800

    organized models:

---
+++ src/train_gender_classifier_branched.py
@@ -0,0 +1,220 @@
+import pandas as pd
+import os
+from PIL import Image
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+
+from keras.utils import to_categorical
+
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Lambda
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+import tensorflow as tf
+
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+
+
+IM_WIDTH = IM_HEIGHT = 198
+
+""" 
+Load Data
+"""
+
+data_path = '../data/raw/'
+image_dir = os.path.join(data_path, 'fairface_pad025')
+
+train_csv = pd.read_csv(os.path.join(data_path, 'fairface_label_train.csv'))
+val_csv = pd.read_csv(os.path.join(data_path, 'fairface_label_val.csv'))
+df = pd.concat([train_csv, val_csv])
+
+
+TRAIN_TEST_SPLIT = 0.7
+IM_WIDTH = IM_HEIGHT = 198
+
+dataset_dict = {
+    'gender_id': {
+        0: 'Male',
+        1: 'Female'
+    }
+}
+
+dataset_dict['gender_alias'] = dict((g, i) for i, g in dataset_dict['gender_id'].items())
+
+class FFFaceDataGenerator():
+    """
+    Data generator for the FFFace dataset. This class should be used when training our Keras multi-output model.
+    """
+    def __init__(self, df):
+        self.df = df
+        
+    def generate_split_indexes(self):
+        p = np.random.permutation(len(self.df))
+        train_up_to = int(len(self.df) * TRAIN_TEST_SPLIT)
+        train_idx = p[:train_up_to]
+        test_idx = p[train_up_to:]
+
+        train_up_to = int(train_up_to * TRAIN_TEST_SPLIT)
+        train_idx, valid_idx = train_idx[:train_up_to], train_idx[train_up_to:]
+        
+        # converts alias to id
+        self.df['gender_id'] = self.df['gender'].map(lambda gender: dataset_dict['gender_alias'][gender])
+        
+        return train_idx, valid_idx, test_idx
+    
+    def preprocess_image(self, img_path):
+        """
+        Used to perform some minor preprocessing on the image before inputting into the network.
+        """
+        im = Image.open(img_path)
+        im = im.resize((IM_WIDTH, IM_HEIGHT))
+        im = np.array(im) / 255.0
+        
+        return im
+        
+    def generate_images(self, image_idx, is_training, batch_size=16):
+        """
+        Used to generate a batch with images when training/testing/validating our Keras model.
+        """
+        
+        # arrays to store our batched data
+        images, genders = [], []
+        while True:
+            for idx in image_idx:
+                person = self.df.iloc[idx]
+                
+                gender = person['gender_id']
+                file = os.path.join(image_dir, person['file'])
+                
+                im = self.preprocess_image(file)
+                
+                genders.append(to_categorical(gender, len(dataset_dict['gender_id'])))
+                images.append(im)
+                
+                # yielding condition
+                if len(images) >= batch_size:
+                    yield np.array(images), np.array(genders)
+                    images, genders = [], []
+                    
+            if not is_training:
+                break
+    def return_df(self):
+        return self.df
+                
+data_generator = FFFaceDataGenerator(df)
+train_idx, valid_idx, test_idx = data_generator.generate_split_indexes()
+
+class FFMultiOutputModel():
+    """
+    Used to generate our multi-output model. This CNN contains three branches, one for age, other for 
+    sex and another for race. Each branch contains a sequence of Convolutional Layers that is defined
+    on the make_default_hidden_layers method.
+    """
+    def make_default_hidden_layers(self, inputs):
+        """
+        Used to generate a default set of hidden layers. The structure used in this network is defined as:
+        
+        Conv2D -> BatchNormalization -> Pooling -> Dropout
+        """
+        x = Conv2D(16, (3, 3), padding="same")(inputs)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = MaxPooling2D(pool_size=(3, 3))(x)
+        x = Dropout(0.25)(x)
+
+        x = Conv2D(32, (3, 3), padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
+
+        x = Conv2D(32, (3, 3), padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=-1)(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
+
+        return x
+
+        return x
+    def build_gender_branch(self, inputs, num_genders=2):
+        """
+        Used to build the gender branch of our face recognition network.
+        This branch is composed of three Conv -> BN -> Pool -> Dropout blocks, 
+        followed by the Dense output layer.
+        """
+        x = Lambda(lambda c: tf.image.rgb_to_grayscale(c))(inputs)
+
+        x = self.make_default_hidden_layers(inputs)
+
+        x = Flatten()(x)
+        x = Dense(128)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.5)(x)
+        x = Dense(num_genders)(x)
+        x = Activation("sigmoid", name="gender_output")(x)
+
+        return x
+
+    def assemble_full_model(self, width, height):
+        """
+        Used to assemble our multi-output model CNN.
+        """
+        input_shape = (height, width, 3)
+
+        inputs = Input(shape=input_shape)
+
+        gender_branch = self.build_gender_branch(inputs)
+
+        model = Model(inputs=inputs,
+                     outputs = gender_branch,
+                     name="face_net")
+
+        return model
+    
+model = FFMultiOutputModel().assemble_full_model(IM_WIDTH, IM_HEIGHT)
+
+init_lr = 1e-4
+epochs = 100
+
+opt = Adam(lr=init_lr, decay=init_lr / epochs)
+
+model.compile(optimizer=opt, 
+              loss={
+                  'gender_output': 'binary_crossentropy'},
+              loss_weights={
+                  'gender_output': 0.1},
+              metrics={
+                  'gender_output': 'accuracy'})
+
+
+batch_size = 32
+valid_batch_size = 32
+train_gen = data_generator.generate_images(train_idx, is_training=True, batch_size=batch_size)
+valid_gen = data_generator.generate_images(valid_idx, is_training=True, batch_size=valid_batch_size)
+
+callbacks = [
+    ModelCheckpoint("../model_checkpoint/gender-cp-{epoch:04d}.hdf5", 
+    monitor='val_loss', 
+    verbose=1, 
+    save_best_only=True, 
+    save_weights_only=False, 
+    mode='auto', 
+    period=1)
+]
+
+history = model.fit_generator(train_gen,
+                    steps_per_epoch=len(train_idx)//batch_size,
+                    epochs=epochs,
+                    callbacks=callbacks,
+                    validation_data=valid_gen,
+                    validation_steps=len(valid_idx)//valid_batch_size)
